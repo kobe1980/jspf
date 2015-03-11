98b34f17f6018fe3ba3e216296aff2b8aba264a6
kobe1980 <kobe1980@gmail.com>
2015-03-11 12:17:35 +0000

    Add Track as an object

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var util = require('util');
 
-function jspf(title, creator, annotation, info, location, identifier, image, date, license, attribution, link, meta, extension, track) {
+function Jspf(title, creator, annotation, info, location, identifier, image, date, license, attribution, link, meta, extension, track) {
 	this.title = title || "";
 	this.creator = creator || "";
 	this.annotation = annotation || "";
@@ -17,249 +17,387 @@ function jspf(title, creator, annotation, info, location, identifier, image, dat
 	this.track = track || Array();
 }
 
-jspf.prototype.setTitle = function(title) {
+Jspf.prototype.setTitle = function(title) {
 	if (!util.isString(title)) return false;
 	this.title = title;
 	return true;
 };
 
-jspf.prototype.getTitle = function() {
+Jspf.prototype.getTitle = function() {
 	return this.title;
 };
 
-jspf.prototype.setCreator = function(creator) {
+Jspf.prototype.setCreator = function(creator) {
 	if (!util.isString(creator)) return false;
 	this.creator = creator;
 	return true;
 };
 
-jspf.prototype.getCreator = function() {
+Jspf.prototype.getCreator = function() {
 	return this.creator;
 };
 
-jspf.prototype.setAnnotation = function(annotation) {
+Jspf.prototype.setAnnotation = function(annotation) {
 	if (!util.isString(annotation)) return false;
 	this.annotation = annotation;
 	return true;
 };
 
-jspf.prototype.getAnnotation = function() {
+Jspf.prototype.getAnnotation = function() {
 	return this.annotation;
 };
 
-jspf.prototype.setInfo = function(info) {
+Jspf.prototype.setInfo = function(info) {
 	if (!util.isString(info)) return false;
 	this.info = info;
 	return true;
 };
 
-jspf.prototype.getInfo = function() {
+Jspf.prototype.getInfo = function() {
 	return this.info;
 };
 
-jspf.prototype.setLocation = function(location) {
+Jspf.prototype.setLocation = function(location) {
 	if (!util.isString(location)) return false;
 	this.location = location;
 	return true;
 };
 
-jspf.prototype.getLocation = function() {
+Jspf.prototype.getLocation = function() {
 	return this.location;
 };
 
-jspf.prototype.setIdentifier = function(identifier) {
+Jspf.prototype.setIdentifier = function(identifier) {
 	if (!util.isString(identifier)) return false;
 	this.identifier = identifier;
 	return true;
 };
 
-jspf.prototype.getIdentifier = function() {
+Jspf.prototype.getIdentifier = function() {
 	return this.identifier;
 };
 
-jspf.prototype.setImage = function(image) {
+Jspf.prototype.setImage = function(image) {
 	if (!util.isString(image)) return false;
 	this.image = image;
 	return true;
 };
 
-jspf.prototype.getImage = function() {
+Jspf.prototype.getImage = function() {
 	return this.image;
 };
 
-jspf.prototype.setDate = function(date) {
+Jspf.prototype.setDate = function(date) {
 	if (!util.isString(date) && ! util.isDate(date)) return false;
 	this.date = date;
 	return true;
 };
 
-jspf.prototype.getDate = function() {
+Jspf.prototype.getDate = function() {
 	return this.date;
 };
 
-jspf.prototype.setLicense = function(license) {
+Jspf.prototype.setLicense = function(license) {
 	if (!util.isString(license)) return false;
 	this.license = license;
 	return true;
 };
 
-jspf.prototype.getLicense = function() {
+Jspf.prototype.getLicense = function() {
 	return this.license;
 };
 
-jspf.prototype.setAttribution = function(attribution) {
+Jspf.prototype.setAttribution = function(attribution) {
 	if (!util.isArray(attribution)) return false;
 	this.attribution = attribution;
 	return true;
 };
 
-jspf.prototype.getAttribution = function() {
+Jspf.prototype.getAttribution = function() {
 	return this.attribution;
 };
 
-jspf.prototype.setLink = function(link) {
+Jspf.prototype.setLink = function(link) {
 	if (!util.isArray(link)) return false;
 	this.link = link;
 	return true;
 };
 
-jspf.prototype.getLink = function() {
+Jspf.prototype.getLink = function() {
 	return this.link;
 };
 
-jspf.prototype.setMeta = function(meta) {
+Jspf.prototype.setMeta = function(meta) {
 	if (!util.isArray(meta)) return false;
 	this.meta = meta;
 	return true;
 };
 
-jspf.prototype.getMeta = function() {
+Jspf.prototype.getMeta = function() {
 	return this.meta;
 };
 
-jspf.prototype.setExtension = function(extension) {
+Jspf.prototype.setExtension = function(extension) {
 	if (!util.isArray(extension)) return false;
 	this.extension = extension;
 	return true;
 };
 
-jspf.prototype.getExtension = function() {
+Jspf.prototype.getExtension = function() {
 	return this.extension;
 };
 
-jspf.prototype.setTrack = function(track) {
+Jspf.prototype.setTrack = function(track) {
 	if (!util.isArray(track)) return false;
 	this.track = track;
 	return true;
 };
 
-jspf.prototype.getTrack = function() {
+Jspf.prototype.getTrack = function() {
 	return this.track;
 };
 
-jspf.prototype.getTrackById = function(trackId) {
+Jspf.prototype.getTrackById = function(trackId) {
 	return this.track[trackId];
 };
 
-jspf.prototype.addTrack = function(location, identifier, title, creator, annotation, info, image, album, trackNum, duration, link, meta, extension) {
-	this.track.push({
-		"location": location,
-		"identifier": identifier,
-		"title": title,
-		"creator": creator,
-		"annotation": annotation,
-		"info": info,
-		"image": image,
-		"album": album,
-		"trackNum": trackNum,
-		"duration": duration,
-		"link": link,
-		"meta": meta,
-		"extension": extension
-	});
+Jspf.prototype.isTrack = function(track) {
+	if (!util.isArray(track)) return false;
+	for (var i in track) {
+		if (!track[i].isTrack()) return false;
+	}
 	return true;
 };
 
-jspf.prototype.setTrackLocation = function(trackId, location) {
+Jspf.prototype.addTrack = function(location, identifier, title, creator, annotation, info, image, album, trackNum, duration, link, meta, extension) {
+	this.track.push(new Track(
+		location,
+		identifier,
+		title,
+		creator,
+		annotation,
+		info,
+		image,
+		album,
+		trackNum,
+		duration,
+		link,
+		meta,
+		extension
+	));
+	return true;
+};
+
+Jspf.prototype.setTrackLocation = function(trackId, location) {
 	if (!util.isNumber(trackId) || !util.isString(location) || trackId > this.track.length || trackId < 0) return false;
-    	this.track[trackId]["location"] = location;
+    	this.track[trackId].setLocation(location);
 	return true;
 };
 
-jspf.prototype.setTrackIdentifier = function(trackId, identifier) {
+Jspf.prototype.setTrackIdentifier = function(trackId, identifier) {
 	if (!util.isNumber(trackId) || !util.isString(identifier) || trackId > this.track.length || trackId < 0) return false;
-    	this.track[trackId]["identifier"] = identifier;
+    	this.track[trackId].setIdentifier(identifier);
 	return true;
 };
 
-jspf.prototype.setTrackTitle = function(trackId, title) {
+Jspf.prototype.setTrackTitle = function(trackId, title) {
 	if (!util.isNumber(trackId) || !util.isString(title) || trackId > this.track.length || trackId < 0) return false;
-    	this.track[trackId]["title"] = title;
+    	this.track[trackId].setTitle(title);
 	return true;
 };
 
-jspf.prototype.setTrackCreator = function(trackId, creator) {
+Jspf.prototype.setTrackCreator = function(trackId, creator) {
 	if (!util.isNumber(trackId) || !util.isString(creator) || trackId > this.track.length || trackId < 0) return false;
-    	this.track[trackId]["creator"] = creator;
+    	this.track[trackId].setCreator(creator);
 	return true;
 };
 
-jspf.prototype.setTrackAnnotation = function(trackId, annotation) {
+Jspf.prototype.setTrackAnnotation = function(trackId, annotation) {
 	if (!util.isNumber(trackId) || !util.isString(annotation) || trackId > this.track.length || trackId < 0) return false;
-    	this.track[trackId]["annotation"] = annotation;
+    	this.track[trackId].setAnnotation(annotation);
 	return true;
 };
 
-jspf.prototype.setTrackInfo = function(trackId, info) {
+Jspf.prototype.setTrackInfo = function(trackId, info) {
 	if (!util.isNumber(trackId) || !util.isString(info) || trackId > this.track.length || trackId < 0) return false;
-    	this.track[trackId]["info"] = info;
+    	this.track[trackId].setInfo(info);
 	return true;
 };
 
-jspf.prototype.setTrackImage = function(trackId, image) {
+Jspf.prototype.setTrackImage = function(trackId, image) {
 	if (!util.isNumber(trackId) || !util.isString(image) || trackId > this.track.length || trackId < 0) return false;
-    	this.track[trackId]["image"] = image;
+    	this.track[trackId].setImage(image);
 	return true;
 };
 
-jspf.prototype.setTrackAlbum= function(trackId, album) {
+Jspf.prototype.setTrackAlbum= function(trackId, album) {
 	if (!util.isNumber(trackId) || !util.isString(album) || trackId > this.track.length || trackId < 0) return false;
-   	this.track[trackId]["album"] = album;
+   	this.track[trackId].setAlbum(album);
 	return true;
 };
 
-jspf.prototype.setTrackTrackNum = function(trackId, trackNum) {
+Jspf.prototype.setTrackTrackNum = function(trackId, trackNum) {
 	if (!util.isNumber(trackId) || !util.isNumber(trackNum) || trackId > this.track.length || trackId < 0) return false;
-    	this.track[trackId]["trackNum"] = trackNum;
+    	this.track[trackId].setTrackNum(trackNum);
 	return true;
 };
 
-jspf.prototype.setTrackDuration = function(trackId, duration) {
+Jspf.prototype.setTrackDuration = function(trackId, duration) {
 	if (!util.isNumber(trackId) || !util.isNumber(duration) || trackId > this.track.length || trackId < 0) return false;
-    	this.track[trackId]["duration"] = duration;
+    	this.track[trackId].setDuration(duration);
 	return true;
 };
 
-jspf.prototype.setTrackLink = function(trackId, link) {
+Jspf.prototype.setTrackLink = function(trackId, link) {
 	if (!util.isNumber(trackId) || !util.isArray(link) || trackId > this.track.length || trackId < 0) return false;
-    	this.track[trackId]["link"] = link;
+    	this.track[trackId].setLink(link);
 	return true;
 };
 
-jspf.prototype.setTrackMeta = function(trackId, meta) {
+Jspf.prototype.setTrackMeta = function(trackId, meta) {
 	if (!util.isNumber(trackId) || !util.isArray(meta) || trackId > this.track.length || trackId < 0) return false;
-    	this.track[trackId]["meta"] = meta;
+    	this.track[trackId].setMeta(meta);
 	return true;
 };
 
-jspf.prototype.setTrackExtension = function(trackId, extension) {
+Jspf.prototype.setTrackExtension = function(trackId, extension) {
 	if (!util.isNumber(trackId) || !util.isObject(extension) || trackId > this.track.length || trackId < 0) return false;
-    	this.track[trackId]["extension"] = extension;
+    	this.track[trackId].setExtension(extension);
 	return true;
 };
 
-jspf.prototype.toString = function() {
+Jspf.prototype.toString = function() {
 	return JSON.stringify(this);
 };
 
-module.exports = jspf;
+function Track(location, identifier, title, creator, annotation, info, image, album, trackNum, duration, link, meta, extension) {
+	this.location = location || "";
+	this.identifier = identifier || "";
+	this.title = title || "";
+	this.creator = creator || "";
+	this.annotation = annotation || "";
+	this.info = info || "";
+	this.image = image || "";
+	this.album = album || "";
+	this.trackNum = trackNum;
+	this.duration = duration;
+	this.link = link || Array();
+	this.meta = meta || Array();
+	this.extension = extension || Array();
+}
+
+Track.prototype.setLocation = function(location) {
+	this.location = location;
+};
+
+Track.prototype.getLocation = function() {
+	return this.location;
+};
+
+Track.prototype.setIdentifier = function(identifier) {
+	this.identifier = identifier;
+};
+
+Track.prototype.getIdentifier = function() {
+	return this.identifier;
+};
+
+Track.prototype.setTitle = function(title) {
+	this.title = title;
+};
+
+Track.prototype.getTitle = function() {
+	return this.title;
+};
+
+Track.prototype.setCreator = function(creator) {
+	this.creator = creator;
+};
+
+Track.prototype.getCreator = function() {
+	return this.creator;
+};
+
+Track.prototype.setAnnotation = function(annotation) {
+	this.annotation = annotation;
+};
+
+Track.prototype.getAnnotation = function() {
+	return this.annotation;
+};
+
+Track.prototype.setInfo = function(info) {
+	this.info = info;
+};
+
+Track.prototype.getInfo = function() {
+	return this.info;
+};
+
+Track.prototype.setImage = function(image) {
+	this.image = image;
+};
+
+Track.prototype.getImage = function() {
+	return this.image;
+};
+
+Track.prototype.setAlbum = function(album) {
+	this.album = album;
+};
+
+Track.prototype.getAlbum = function() {
+	return this.album;
+};
+
+Track.prototype.setTrackNum = function(trackNum) {
+	this.trackNum = trackNum;
+};
+
+Track.prototype.getTrackNum = function() {
+	return this.trackNum;
+};
+
+Track.prototype.setDuration = function(duration) {
+	this.duration = duration;
+};
+
+Track.prototype.getDuration = function() {
+	return this.duration;
+};
+
+Track.prototype.setLink = function(link) {
+	this.link = link;
+};
+
+Track.prototype.getLink = function() {
+	return this.link;
+};
+
+Track.prototype.setMeta = function(meta) {
+	this.meta = meta;
+};
+
+Track.prototype.getMeta = function() {
+	return this.meta;
+};
+
+Track.prototype.setExtension = function(extension) {
+	this.extension = extension;
+};
+
+Track.prototype.getExtension = function() {
+	return this.extension;
+};
+
+Track.prototype.isTrack = function(track) {
+	console.log(Object.prototype.toString.call(track));
+};
+
+Track.prototype.toString = function() {
+	return JSON.stringify(this);
+};
+
+module.exports = Track;
+module.exports = Jspf;
+
